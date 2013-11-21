bfeb8cf816d64b77342adc5caac12aecf6ef4381
Aparajita Fishman <aparajita@aparajita.com>
2013-11-21 18:41:23 +0000

    Initial commit

---
+++ gjslint.py
@@ -0,0 +1,31 @@
+#
+# gjslint.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Aparajita Fishman
+#
+# Project: https://github.com/SublimeLinter/SublimeLinter-contrib-gjslint
+# License: MIT
+#
+
+from SublimeLinter.lint import Linter
+
+
+class GJSLint(Linter):
+    language = 'javascript'
+    cmd = ('gjslint', '--nobeep', '--nosummary')
+    regex = r'^Line (?P<line>\d+), (?P<type>.):\d+: (?P<error>[^"]+(?P<near>"[^"]+")?)$'
+    comment_re = r'\s*/[/*]'
+    defaults = {
+        '--jslint_error:,+': "",
+        '--disable:,': "",
+        '--max_line_length:': None
+    }
+    inline_settings = 'max_line_length'
+    inline_overrides = 'disable'
+    tempfile_suffix = 'js'
+
+
+class EmbeddedGJSLint(GJSLint):
+    language = 'html'
+    selector = 'source.js.embedded.html'
