45ba7a4e49be77d512f7ffffd623c32bdeff8761
Aparajita Fishman <aparajita@aparajita.com>
2013-11-26 11:14:16 -0500

    PEP257 docstrings

--- gjslint.py
+++ gjslint.py
@@ -8,10 +8,15 @@
 # License: MIT
 #
 
+"""This module exports the GJSLint plugin linter class."""
+
 from SublimeLinter.lint import Linter
 
 
 class GJSLint(Linter):
+
+    """Provides an interface to the gjslint executable."""
+
     language = ('javascript', 'html')
     cmd = 'gjslint --nobeep --nosummary'
     regex = r'^Line (?P<line>\d+), (?:(?P<error>E)|(?P<warning>W)):\d+: (?P<message>[^"]+(?P<near>"[^"]+")?)$'
