b571cfb9bb80f2c2bab48628632683c7fa9d8099
Gaston Sanchez <gaston_sanchez@intuit.com>
2016-04-07 11:58:57 -0500

    Add JavaScript (Babel) syntax

--- linter.py
+++ linter.py
@@ -17,7 +17,7 @@ class GJSLint(Linter):
 
     """Provides an interface to the gjslint executable."""
 
-    syntax = ('javascript', 'javascriptnext', 'html')
+    syntax = ('javascript', 'javascript (babel)', 'javascriptnext', 'html')
     cmd = 'gjslint --nobeep --nosummary'
     regex = r'^Line (?P<line>\d+), (?:(?P<error>E)|(?P<warning>W)):\d+: (?P<message>[^"]+(?P<near>"[^"]+")?.*$)'
     comment_re = r'\s*/[/*]'
